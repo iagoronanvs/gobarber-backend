9e1d281302fa2ae53bb8f6a36393cb7c9e6afe3e
iagoronanvs <iago.vsantos@gmail.com>
2020-10-18 15:36:06 -0300

    teste atualização de avatar

---
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,95 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHasProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+import CreateUserService from './CreateUserService';
+
+describe('UpdateUserAvatar', () => {
+  it('should be able to change user avatar', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await createUserService.execute({
+      name: 'Jhon Doe',
+      email: 'jhon@example.com',
+      password: 'password',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.png',
+    });
+
+    expect(user).toHaveProperty('avatar');
+    expect(user.avatar).toBe('avatar.png');
+  });
+
+  it('should not be able update avatar from non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatar.execute({
+        user_id: '123123123',
+        avatarFilename: 'avatar.png',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should delete old avatar when updating new one ', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await createUserService.execute({
+      name: 'Jhon Doe',
+      email: 'jhon@example.com',
+      password: 'password',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.png',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'newAvatar.png',
+    });
+
+    expect(user).toHaveProperty('avatar');
+    expect(user.avatar).toBe('newAvatar.png');
+    expect(deleteFile).toHaveBeenCalledWith('avatar.png');
+  });
+});
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,39 +1,41 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 
-import express, { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
-import routes from "./routes";
+import routes from './routes';
 
-import "@shared/infra/typeorm";
-import "@shared/container";
+import '@shared/infra/typeorm';
+import '@shared/container';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
       status: 'error',
-      message: err.message
-    })
-  }
-
-  console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error'
-  });
-});
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333.");
+  console.log('Server started on port 3333.');
 });

44ef844fc4d6876998587a042b790f7ff56e32fc
iagoronanvs <iago.vsantos@gmail.com>
2020-11-10 22:26:25 -0300

    listagem de prestadores

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
-import { parseISO } from "date-fns";
-import { container } from "tsyringe";
+import { parseISO } from 'date-fns';
+import { container } from 'tsyringe';
 
-import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService";
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 export default class AppointmentController {
   public async create(request: Request, response: Response): Promise<Response> {
